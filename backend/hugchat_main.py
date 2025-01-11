5ee890177c3988c50837e93a24798c33c6cb2fc9
Fustigate8933 <ianchang891@gmail.com>
2025-01-11 17:25:19 -0500

    switched to login per user

--- backend/hugchat_main.py
+++ backend/hugchat_main.py
@@ -1,43 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from system_prompt import system_prompt
 from hugchat import hugchat
 from hugchat.login import Login
-from hugchat.hugchat import MessageNode, Conversation
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 app = FastAPI()
 
-load_dotenv()
+# load_dotenv()
 
-EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("PASSWORD")
+# EMAIL = os.getenv("EMAIL")
+# PASSWORD = os.getenv("PASSWORD")
+
+# try:
+#     chatbot.delete_all_conversations()
+#     print("All conversations removed successfully.")
+# except Exception as e:
+#     print(e)
 
 cookie_path_dir = "./cookies/"
-sign = Login(EMAIL, PASSWORD)
-cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
-
-chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-
-try:
-    chatbot.delete_all_conversations()
-    print("All conversations removed successfully.")
-except Exception as e:
-    print(e)
-
-chatbot.new_conversation(switch_to=True, system_prompt=system_prompt)
 
 class GenRequest(BaseModel):
     query: str
+    email: str
+    password: str
 
 @app.post("/")
-async def generate_breakdown(request: GenRequest):
-    print("Processing request...")
+def generate_breakdown(request: GenRequest):
+
+    sign = Login(request.email, request.password)
+    cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    chatbot.new_conversation(switch_to=True, system_prompt=system_prompt)
+    
     try:
+        print("Generating breakdown...")
         message = chatbot.chat(request.query).wait_until_done()
     except Exception as e:
-        print("An error has occured while processing the request with hugchat: ", e)
+        raise HTTPException(status_code=500, detail=f"Error while processing the query: {e}")
 
     return JSONResponse({"breakdown": message})
+
+
+# import time
+# @app.post("/")
+# def generate_breakdown(request: GenRequest):
+#     print("hi")
+#     time.sleep(3)
+#     print("bye")
