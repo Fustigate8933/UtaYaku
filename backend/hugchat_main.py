0e8c0657b0e2c6f85c9f280cd845c37b3b9c0280
Fustigate8933 <ianchang891@gmail.com>
2025-01-13 18:12:59 -0500

    removed unused comments

--- backend/hugchat_main.py
+++ backend/hugchat_main.py
@@ -4,21 +4,8 @@ from pydantic import BaseModel
 from system_prompt import system_prompt
 from hugchat import hugchat
 from hugchat.login import Login
-# import os
-# from dotenv import load_dotenv
 
 app = FastAPI()
-
-# load_dotenv()
-
-# EMAIL = os.getenv("EMAIL")
-# PASSWORD = os.getenv("PASSWORD")
-
-# try:
-#     chatbot.delete_all_conversations()
-#     print("All conversations removed successfully.")
-# except Exception as e:
-#     print(e)
 
 cookie_path_dir = "./cookies/"
 
@@ -42,10 +29,3 @@ def generate_breakdown(request: GenRequest):
     except Exception as e:
         print({"breakdown": str(e)})
         return JSONResponse(status_code=500, content={"breakdown": str(e)})
-
-# import time
-# @app.post("/")
-# def generate_breakdown(request: GenRequest):
-#     print("hi")
-#     time.sleep(3)
-#     print("bye")
