bc7bae938b11d4e0723dc13332b5521dc22dc231
Fustigate8933 <ianchang891@gmail.com>
2025-02-01 20:04:11 -0500

    option to use openai, testing ichiran

--- backend/hugchat_main.py
+++ backend/hugchat_main.py
@@ -20,9 +20,10 @@ def generate_breakdown(request: GenRequest):
         sign = Login(request.email, request.password)
         cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
         chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+        llm = 3
+        chatbot.switch_llm(llm)
         chatbot.new_conversation(switch_to=True, system_prompt=system_prompt)
-
-        print("Generating breakdown...")
+        print(f"Generating breakdown via {chatbot.get_available_llm_models()[llm].__dict__["name"]}")
 
         message = chatbot.chat(request.query).wait_until_done()
         return JSONResponse(status_code=200, content={"breakdown": message})
